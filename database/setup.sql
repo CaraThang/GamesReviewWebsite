0d7615c35dfdd29998bc46b620a7ccff636aad9f
CaraThang <cara.thang@icloud.com>
2024-12-08 20:14:54 +1100

    Added Database

---
+++ database/setup.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL, 
+    password TEXT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS entries(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL, 
+    game_id INTEGER NOT NULL, 
+    title TEXT NOT NULL,
+    description TEXT,
+    rating INTEGER NOT NULL,
+    image_path TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (game_id) REFERENCES games(id));
+
+CREATE TABLE IF NOT EXISTS games(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    year INTEGER NOT NULL, 
+    description TEXT,
+    rating INTEGER NOT NULL,
+    image_path TEXT NOT NULL);
